6bc46b1fc4217e815e9b2ac80dd977b73b4e2574
Lightthouse <45074588+Lightthouse@users.noreply.github.com>
2018-11-23 13:10:32 +0500

    кнопка к анимации

--- анимация/script/script.js
+++ анимация/script/script.js
@@ -1,12 +1,33 @@
 let div=document.getElementById(`mom`);
 let cir=document.getElementById(`son`);
+let butt=document.getElementById(`butt`);
+
 let count=0;
 let incr=1;
+let go=false;
 
-let id=setInterval( ()=>{
-	cir.style.top=`${count}px`;
-	cir.style.left=`${count}px`;
-	count+=incr;
-	if(count>=320)incr=-1;
-	if(count<=0)incr=1;
-})
+
+butt.onclick=()=>{
+	if(go==true){
+		go=false;
+		butt.textContent="Go!";
+	}
+	else{
+		go=true;
+		butt.textContent="Stop!";
+	}
+	
+	move();
+
+}
+
+function move(){
+		let id=setInterval( ()=>{
+		cir.style.top=`${count}px`;
+		cir.style.left=`${count}px`;
+		count+=incr;
+		if(count>=320)incr=-1;
+		if(count<=0)incr=1;
+		if(go==false)clearInterval(id)
+	})
+}
